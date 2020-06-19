c8b8d3bb62ab19591460ebed9afe32d3ccd4ca87
Stella Xu <stellaxu3116@gmail.com>
2020-06-19 14:42:39 -0400

    added SimpleReadInt

--- keyboard.go
+++ keyboard.go
@@ -22,7 +22,24 @@ func ReadInt() (int, error) {
 	guess, err := strconv.Atoi(input)
 	return guess, err
 }
+func SimpleReadInt() int {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	input = strings.TrimSpace(input)
+	//fmt.Println(len(input))
+
+	// Atoi converts a string into an integer
+	guess, err := strconv.Atoi(input)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return guess
+}
 func ReadFloat64() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
